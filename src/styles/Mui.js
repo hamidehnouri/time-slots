d4f5aad638bcae0195c1dc88ffba4076b873cf87
Hamideh Nouri <nouri.hamideh@gmail.com>
2021-08-29 23:07:11 +0200

    Theme color is customized

---
+++ src/styles/Mui.js
@@ -0,0 +1,12 @@
+import {createTheme} from "@material-ui/core/styles"
+
+export const theme = createTheme({
+    palette: {
+        primary: {
+            main: "#005039"
+        },
+        secondary: {
+            main: "#e8b000",
+        }
+    }
+})
